8572c7f41e28ecad5e7683c525740d9706d9a5ec
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2023-08-04 13:33:37 +0530

    added usermodel for registration

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,11 +1,23 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from "axios";
 
 const RegisterPage = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    function registerUser() {
+    async function registerUser(e) {
+        e.preventDefault();
+        try {
+            await axios.post('/register', {
+                name,
+                email,
+                password,
+            })
+            alert('Registration Successful, Now You can Log in')
+        } catch (e) {
+            alert('Registration Failed, please try again later')
+        }
     }
     return (
         <div className="mt-4 grow flex items-center justify-around">
@@ -29,7 +41,7 @@ const RegisterPage = () => {
                         value={password}
                         onChange={ev => setPassword(ev.target.value)}
                     />
-                    <button className="primary">Login</button>
+                    <button className="primary">Register</button>
                     <div className="text-center py-2 text-gray-500">
                         Already a member?
                         <Link to={'/login'} className="underline text-black">Login</Link>
