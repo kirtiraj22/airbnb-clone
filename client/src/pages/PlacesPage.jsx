5bc5634c4d8868138c968ba1343606728790dbdd
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2023-08-11 22:53:15 +0530

    added states for accomodations

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,7 +1,41 @@
+import { useState } from "react";
 import { Link, useParams } from "react-router-dom"
+import Perks from "../Perks";
 
 const PlacesPage = () => {
     const { action } = useParams();
+    const [title, setTitle] = useState('')
+    const [address, setAddress] = useState('')
+    const [addedPhotos, setAddressPhotos] = useState([])
+    const [photoLink, setPhotoLink] = useState('')
+    const [description, setDescription] = useState('')
+    const [perks, setPerks] = useState([])
+    const [extraInfo, setExtraInfo] = useState('')
+    const [checkIn, setCheckIn] = useState('')
+    const [checkOut, setCheckOut] = useState('')
+    const [maxGuests, setMaxGuests] = useState(1)
+
+    function inputHeader(text) {
+        return (
+            <span className="text-2xl mt-4 font-semibold">{text}</span>
+        )
+    }
+
+    function inputDescription(text) {
+        return (
+            <p className="text-gray-500 text-sm">{text}</p>
+        )
+    }
+
+    function preInput(header, description) {
+        return (
+            <>
+                {inputHeader(header)}
+                {inputDescription(description)}
+            </>
+        )
+    }
+
     return (
         <div className="">
             {action !== "new" && (
@@ -18,16 +52,13 @@ const PlacesPage = () => {
             {action === 'new' && (
                 <div>
                     <form>
-                        <span className="text-2xl mt-4 font-semibold">Title</span>
-                        <p className="text-gray-500 text-sm">Title for your places should be short and catchy as in advertisement</p>
-                        <input type="text" placeholder="title, for eg: My lovely apt" />
-                        <span className="text-2xl mt-4 font-semibold">Address</span>
-                        <p className="text-gray-500 text-sm">Address to this place</p>
-                        <input type="text" placeholder="address" />
-                        <span className="text-2xl mt-4 font-semibold">Photos</span>
-                        <p className="text-gray-500 text-sm">more = better</p>
+                        {preInput('Title', 'Title for your places should be short and catchy as in advertisement')}
+                        <input type="text" value={title} onChange={ev => setTitle(ev.target.value)} placeholder="title, for eg: My lovely apt" />
+                        {preInput('Address', 'Address to this place')}
+                        <input type="text" value={address} onChange={ev => setAddress(ev.target.value)} placeholder="address" />
+                        {preInput('Photos', 'more = better')}
                         <div className="flex gap-2">
-                            <input type="text" placeholder="add using a link ...jpeg" />
+                            <input type="text" value={photoLink} onChange={ev => setPhotoLink(ev.target.value)} placeholder="add using a link ...jpeg" />
                             <button className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
                         </div>
                         <div className="mt-2 grid grid-cols-3 md:cols-4 lg:grid-cols-6">
@@ -39,6 +70,32 @@ const PlacesPage = () => {
                                 Upload
                             </button>
                         </div>
+                        {preInput('Description', 'desciption of the place')}
+                        <textarea value={description} onChange={ev => setDescription(ev.target.value)} />
+                        {preInput('Perks', 'Select all the perks of your place')}
+
+                        <div className="grid mt-2 gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
+                            <Perks selected={perks} onChange={setPerks} />
+                        </div>
+
+                        {preInput('Extra info', 'house rules, etc')}
+                        <textarea value={extraInfo} onChange={ev => setExtraInfo(ev.target.value)} />
+                        {preInput('Check in&out times', 'add checks in and out times, remember to have some time window for cleaning the room between guests')}
+                        <div className="grid gap-3 sm:grid-cols-3">
+                            <div>
+                                <span className="mt-2 -mb-1">Check in time</span>
+                                <input type="text" value={checkIn} onChange={ev => setCheckIn(ev.target.value)} placeholder="14" />
+                            </div>
+                            <div>
+                                <span className="mt-2 -mb-1">Check out time</span>
+                                <input type="text" placeholder="22" value={checkOut} onChange={ev => setCheckOut(ev.target.value)} />
+                            </div>
+                            <div>
+                                <span className="mt-2 -mb-1">Max number of guests</span>
+                                <input type="number" placeholder="1" value={maxGuests} onChange={ev => setMaxGuests(ev.target.value)} />
+                            </div>
+                        </div>
+                        <button className="primary my-4">Save</button>
                     </form>
                 </div>
 
