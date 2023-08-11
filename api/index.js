5255cb2bd10340bd133b9f3cdc6e0294c2af53ec
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2023-08-11 23:35:10 +0530

    added endpoint for image(upload by link)

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("./models/User.js");
 const cookieParser = require("cookie-parser");
+const imageDownloader = require("image-downloader");
 require("dotenv").config();
 const app = express();
 
@@ -13,7 +14,7 @@ const jwtSecret = "fascawk213casnxczxczsadcxz";
 
 app.use(express.json());
 app.use(cookieParser());
-
+app.use("/uploads", express.static(__dirname + "/uploads"));
 app.use(
   cors({
     credentials: true,
@@ -78,6 +79,17 @@ app.get("/profile", (req, res) => {
 
 app.post("/logout", (req, res) => {
   res.cookie("token", "").json(true);
+});
+
+app.post("/upload-by-link", async (req, res) => {
+  const { link } = req.body;
+  const newName = "photo" + Date.now() + ".jpg";
+  await imageDownloader.image({
+    url: link,
+    dest: __dirname + `/uploads/+${newName}`,
+  });
+
+  res.json(newName);
 });
 
 app.listen(4000);
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
+import axios from "axios"
 import { Link, useParams } from "react-router-dom"
 import Perks from "../Perks";
-
 const PlacesPage = () => {
     const { action } = useParams();
     const [title, setTitle] = useState('')
     const [address, setAddress] = useState('')
-    const [addedPhotos, setAddressPhotos] = useState([])
+    const [addedPhotos, setAddedPhotos] = useState([])
     const [photoLink, setPhotoLink] = useState('')
     const [description, setDescription] = useState('')
     const [perks, setPerks] = useState([])
@@ -36,6 +36,15 @@ const PlacesPage = () => {
         )
     }
 
+    async function addPhotoByLink(ev) {
+        ev.preventDefault();
+        const { data: filename } = await axios.post('/upload-by-link', { link: photoLink })
+        setAddedPhotos(prev => {
+            return [...prev, filename]
+        })
+        setPhotoLink('')
+    }
+
     return (
         <div className="">
             {action !== "new" && (
@@ -59,9 +68,16 @@ const PlacesPage = () => {
                         {preInput('Photos', 'more = better')}
                         <div className="flex gap-2">
                             <input type="text" value={photoLink} onChange={ev => setPhotoLink(ev.target.value)} placeholder="add using a link ...jpeg" />
-                            <button className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
+                            <button onClick={addPhotoByLink} className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
                         </div>
                         <div className="mt-2 grid grid-cols-3 md:cols-4 lg:grid-cols-6">
+                            {
+                                addedPhotos.length > 0 && addedPhotos.map(link => (
+                                    <div>
+                                        <img src={`http://localhost:4000/uploads/${link}`} alt="image" />
+                                    </div>
+                                ))
+                            }
                             <button className="flex justify-center gap-1 border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
