d0ac0a7944c039009148e5df69d76417d55d94ee
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2023-08-16 10:53:48 +0530

    fixed bugs && reformatted the code

---
+++ client/src/pages/PlacesFormPage.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react'
+import PhotosUploader from '../PhotosUploader'
+import Perks from '../Perks'
+import AccountNav from '../AccountNav'
+import { Navigate } from 'react-router-dom'
+import axios from 'axios'
+
+const PlacesFormPage=() => {
+    
+    const [title, setTitle] = useState('')
+    const [address, setAddress] = useState('')
+    const [addedPhotos, setAddedPhotos] = useState([])
+    const [description, setDescription] = useState('')
+    const [perks, setPerks] = useState([])
+    const [extraInfo, setExtraInfo] = useState('')
+    const [checkIn, setCheckIn] = useState('')
+    const [checkOut, setCheckOut] = useState('')
+    const [maxGuests, setMaxGuests] = useState(1)
+    const [redirect, setRedirect] = useState(false)
+
+    function inputHeader(text) {
+        return (
+            <span className="text-2xl mt-4 font-semibold">{text}</span>
+        )
+    }
+
+    function inputDescription(text) {
+        return (
+            <p className="text-gray-500 text-sm">{text}</p>
+        )
+    }
+
+    function preInput(header, description) {
+        return (
+            <>
+                {inputHeader(header)}
+                {inputDescription(description)}
+            </>
+        )
+    }
+
+    async function addNewPlace(ev){
+        ev.preventDefault();
+        await axios.post('/places', { 
+            title, address, addedPhotos,
+            description, perks, extraInfo, 
+            checkIn, checkOut, maxGuests
+        })
+        setRedirect(true)
+    }
+
+    if(redirect){
+        return <Navigate to={'/account/places'} />
+    }
+
+    return (
+        <div>
+            <AccountNav />
+            <form onSubmit={addNewPlace}>
+                {preInput('Title', 'Title for your places should be short and catchy as in advertisement')}
+                <input type="text" value={title} onChange={ev => setTitle(ev.target.value)} placeholder="title, for eg: My lovely apt" />
+                {preInput('Address', 'Address to this place')}
+                <input type="text" value={address} onChange={ev => setAddress(ev.target.value)} placeholder="address" />
+                {preInput('Photos', 'more = better')}
+
+                <PhotosUploader addedPhotos={addedPhotos} onChange={setAddedPhotos} />
+
+                {preInput('Description', 'desciption of the place')}
+                <textarea value={description} onChange={ev => setDescription(ev.target.value)} />
+                {preInput('Perks', 'Select all the perks of your place')}
+
+                <div className="grid mt-2 gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
+                    <Perks selected={perks} onChange={setPerks} />
+                </div>
+
+                {preInput('Extra info', 'house rules, etc')}
+                <textarea value={extraInfo} onChange={ev => setExtraInfo(ev.target.value)} />
+                {preInput('Check in&out times', 'add checks in and out times, remember to have some time window for cleaning the room between guests')}
+                <div className="grid gap-3 sm:grid-cols-3">
+                    <div>
+                        <span className="mt-2 -mb-1">Check in time</span>
+                        <input type="text" value={checkIn} onChange={ev => setCheckIn(ev.target.value)} placeholder="14" />
+                    </div>
+                    <div>
+                        <span className="mt-2 -mb-1">Check out time</span>
+                        <input type="text" placeholder="22" value={checkOut} onChange={ev => setCheckOut(ev.target.value)} />
+                    </div>
+                    <div>
+                        <span className="mt-2 -mb-1">Max number of guests</span>
+                        <input type="number" placeholder="1" value={maxGuests} onChange={ev => setMaxGuests(ev.target.value)} />
+                    </div>
+                </div>
+                <button className="primary my-4">Save</button>
+            </form>
+        </div>
+    )
+}
+
+export default PlacesFormPage
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,71 +1,15 @@
 import { useState } from "react";
-import axios from "axios"
-import { Link, useParams } from "react-router-dom"
-import Perks from "../Perks";
-const PlacesPage = () => {
-    const { action } = useParams();
-    const [title, setTitle] = useState('')
-    const [address, setAddress] = useState('')
-    const [addedPhotos, setAddedPhotos] = useState([])
-    const [photoLink, setPhotoLink] = useState('')
-    const [description, setDescription] = useState('')
-    const [perks, setPerks] = useState([])
-    const [extraInfo, setExtraInfo] = useState('')
-    const [checkIn, setCheckIn] = useState('')
-    const [checkOut, setCheckOut] = useState('')
-    const [maxGuests, setMaxGuests] = useState(1)
-
-    function inputHeader(text) {
-        return (
-            <span className="text-2xl mt-4 font-semibold">{text}</span>
-        )
-    }
-
-    function inputDescription(text) {
-        return (
-            <p className="text-gray-500 text-sm">{text}</p>
-        )
-    }
-
-    function preInput(header, description) {
-        return (
-            <>
-                {inputHeader(header)}
-                {inputDescription(description)}
-            </>
-        )
-    }
-
-    async function addPhotoByLink(ev) {
-        ev.preventDefault();
-        const { data: filename } = await axios.post('/upload-by-link', { link: photoLink })
-        setAddedPhotos((prev) => {
-            return [...prev, filename]
-        })
-        setPhotoLink('')
-    }
-
-    function uploadPhoto(ev){
-        const files = ev.target.files;
-        const data = new FormData();
-        for(let i = 0; i < files.length; i++){
-            data.append('photos', files[i])
-        }
-        axios.post('/upload', data, {
-            headers: {'Content-type': 'multipart/form-data'}
-        }).then(response => {
-            const {data:filenames} = response;
-            setAddedPhotos(prev => {
-                return [...prev, ...filenames]
-            })
-
-        })
-    }
+import { Link } from "react-router-dom"
+import AccountNav from "../AccountNav";
+const PlacesPage=() => {
+    const [redirectToPlacesList, setRedirectToPlacesList]=useState(false)
 
     return (
-        <div className="">
-            {action !== "new" && (
+        <div>
+            <AccountNav />
                 <div className="text-center">
+                    list of all added places
+                    <br />
                     <Link className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full"
                         to={'/account/places/new'}>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -74,67 +18,6 @@ const PlacesPage = () => {
                         Add new Places
                     </Link>
                 </div>
-            )}
-            {action === 'new' && (
-                <div>
-                    <form>
-                        {preInput('Title', 'Title for your places should be short and catchy as in advertisement')}
-                        <input type="text" value={title} onChange={ev => setTitle(ev.target.value)} placeholder="title, for eg: My lovely apt" />
-                        {preInput('Address', 'Address to this place')}
-                        <input type="text" value={address} onChange={ev => setAddress(ev.target.value)} placeholder="address" />
-                        {preInput('Photos', 'more = better')}
-                        <div className="flex gap-2">
-                            <input type="text" value={photoLink} onChange={ev => setPhotoLink(ev.target.value)} placeholder="add using a link ...jpeg" />
-                            <button onClick={addPhotoByLink} className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
-                        </div>
-
-                        <div className="mt-2 gap-2 grid grid-cols-3 md:cols-4 lg:grid-cols-6">
-                            {
-                                addedPhotos.length > 0 && addedPhotos.map(link => (
-                                    <div className="h-32 flex">
-                                        <img className="rounded-2xl w-full object-cover" src={`http://localhost:4000/uploads/${link}`} alt="image" />
-                                    </div>
-                                ))
-                            }
-                            <label className="h-32 flex items-center justify-center gap-1 border bg-transparent rounded-2xl p-2 text-2xl text-gray-600 cursor-pointer">
-                                <input type="file" multiple className="hidden" onChange={uploadPhoto}/>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
-                                </svg>
-
-                                Upload
-                            </label>
-                        </div>
-                        {preInput('Description', 'desciption of the place')}
-                        <textarea value={description} onChange={ev => setDescription(ev.target.value)} />
-                        {preInput('Perks', 'Select all the perks of your place')}
-
-                        <div className="grid mt-2 gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
-                            <Perks selected={perks} onChange={setPerks} />
-                        </div>
-
-                        {preInput('Extra info', 'house rules, etc')}
-                        <textarea value={extraInfo} onChange={ev => setExtraInfo(ev.target.value)} />
-                        {preInput('Check in&out times', 'add checks in and out times, remember to have some time window for cleaning the room between guests')}
-                        <div className="grid gap-3 sm:grid-cols-3">
-                            <div>
-                                <span className="mt-2 -mb-1">Check in time</span>
-                                <input type="text" value={checkIn} onChange={ev => setCheckIn(ev.target.value)} placeholder="14" />
-                            </div>
-                            <div>
-                                <span className="mt-2 -mb-1">Check out time</span>
-                                <input type="text" placeholder="22" value={checkOut} onChange={ev => setCheckOut(ev.target.value)} />
-                            </div>
-                            <div>
-                                <span className="mt-2 -mb-1">Max number of guests</span>
-                                <input type="number" placeholder="1" value={maxGuests} onChange={ev => setMaxGuests(ev.target.value)} />
-                            </div>
-                        </div>
-                        <button className="primary my-4">Save</button>
-                    </form>
-                </div>
-
-            )}
         </div>
     )
 }
